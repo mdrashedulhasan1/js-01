efb00b79b54d67b613d0c7025fc817d9a817f713
Rashedul Hasan <Rashedul Hasan>
2021-09-05 02:27:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// var num = 12;
+// console.log(typeof num);
+
+// var name = 'tapu';
+// console.log(name1);
+// console.log(typeof name1);
+
+// var isGood = true;
+// var isBad = false;
+// console.log(typeof isGood);
+
+var love = "I love JavaScript";
+// // console.log(love.toUpperCase());
+// console.log(love.toLowerCase());
+// console.log(love.indexOf("J"));
+
+var number = "1246247924";
+console.log(number.split('2'));
